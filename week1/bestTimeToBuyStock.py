6d0e85e93de8f07af4b754cc4773f24b3e119579
Nozim <mukhammadnozim.islamov@gmail.com>
2022-07-02 10:24:14 -0700

    added solution to best Time to buy stock

--- week1/bestTimeToBuyStock.py
+++ week1/bestTimeToBuyStock.py
@@ -1,2 +1,15 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
+def maxProfit(prices):
+    minPrice = prices[0]
+    profit = 0
+
+    for i in range(1, len(prices)):
+        if prices[i] - minPrice > profit:
+            profit = prices[i] - minPrice
+        if prices[i] < minPrice:
+            minPrice = prices[i]
+    
+    return profit
+
+print(maxProfit([7,6,4,3,1]))
